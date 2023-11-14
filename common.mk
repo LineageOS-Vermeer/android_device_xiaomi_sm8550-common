38cb5a0aa9c4d36b4a7b3c2635499ee37ad2fee3
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-11-14 18:12:52 +0100

    sm8550-common: Add support for ESIM (eUICC)
    Change-Id: I9a1f820eaa454cbc48732f9b911d228287a390ea

--- common.mk
+++ common.mk
@@ -371,7 +371,10 @@ PRODUCT_PACKAGES += \
     Xiaomi8550CarrierConfigRes \
     Xiaomi8550CarrierConfigResMiui \
     Xiaomi8550DeviceAsWebcamRes \
+    Xiaomi8550EuiccGoogle \
     Xiaomi8550Frameworks \
+    Xiaomi8550FrameworksAOSPA \
+    Xiaomi8550FrameworksEuicc \
     Xiaomi8550Nfc \
     Xiaomi8550SecureElement \
     Xiaomi8550Settings \
@@ -475,7 +478,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.hardware.telephony.mbms.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.mbms.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-euiccgoogle.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-euiccgoogle.xml
 
 # Thermal
 PRODUCT_PACKAGES += \

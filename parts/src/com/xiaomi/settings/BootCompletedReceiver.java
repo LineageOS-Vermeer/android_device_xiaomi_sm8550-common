fabbfc42f0b0f0c758ab0956937d96d88b298f93
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-07-07 19:29:42 +0200

    sm8550-common: Add support for edge rejection
     * inspired by https://github.com/chrmhoffmann/miui-services/blob/main/sources/com/miui/server/input/edgesuppression/EdgeSuppressionManager.java
    
    Co-authored-by: Akash Srivastava <akashniki@gmail.com>
    Change-Id: I93373155103fa8f318c503a6713c47bcf0cd2add

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.java
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.java
@@ -17,6 +17,7 @@ import android.view.Display;
 import android.view.Display.HdrCapabilities;
 
 import com.xiaomi.settings.display.ColorModeService;
+import com.xiaomi.settings.edgesuppression.EdgeSuppressionService;
 import com.xiaomi.settings.touch.TouchOrientationService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -43,6 +44,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
                 UserHandle.CURRENT);
 
         // Touchscreen
+        context.startServiceAsUser(new Intent(context, EdgeSuppressionService.class),
+                UserHandle.CURRENT);
         context.startServiceAsUser(new Intent(context, TouchOrientationService.class),
                 UserHandle.CURRENT);
         

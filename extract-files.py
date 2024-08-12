9808d897a7280bbc52a08400cb1f9f278ba03b4b
danielml <daniel@danielml.dev>
2024-08-12 15:08:10 +0200

    sm8550-common: Import libsdm-color.so from rtwo
    Stock libsdm-color.so uses an outdated PPHWAttributes struct, which is
    missing a field, causing the panel_name and subsequent attributes to be
    misaligned.
    
    Change-Id: Ic19385d8def721ce9bb714f047ca4b54a10bc64d
    Signed-off-by: danielml <daniel@danielml.dev>

--- extract-files.py
+++ extract-files.py
@@ -47,7 +47,6 @@ lib_fixups: lib_fixups_user_type = {
     ): lib_fixup_odm_suffix,
     (
         'audio.primary.kalama',
-        'libsdm-color',
         'libsdm-disp-vndapis',
         'libsdmextension',
     ): lib_fixup_sm8550_suffix,

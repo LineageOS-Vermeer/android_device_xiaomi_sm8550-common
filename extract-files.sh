0b69934456bf7d55b074e8ddfa469ca64a8bd664
Arian <arian.kulmer@web.de>
2024-09-06 14:01:17 +0200

    sm8550-common: Patch vendor.libdpmframework.so to use libhidlbase_shim
    Change-Id: Iefe50b82b5b13bbdee10819a0c60c14240fab7ce

--- extract-files.sh
+++ extract-files.sh
@@ -112,6 +112,10 @@ function blob_fixup() {
         vendor/lib64/c2.dolby.client.so)
             "${PATCHELF}" --add-needed "dolbycodec_shim.so" "${2}"
             ;;
+        vendor/lib64/vendor.libdpmframework.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;

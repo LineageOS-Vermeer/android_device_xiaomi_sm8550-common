7639853565d729c8d00524318304a7ed9782ebf1
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-11-07 09:01:55 +0100

    sm8550-common: Remove non common blob fixups
     * lets add them to device trees if needed
    
    Change-Id: I55e015c9e9791d644272585298734d6b54eb5022

--- extract-files.sh
+++ extract-files.sh
@@ -71,21 +71,9 @@ function blob_fixup() {
         product/etc/permissions/vendor.qti.hardware.data.connection-V1*.xml)
             sed -i 's/xml version="2.0"/xml version="1.0"/g' "${2}"
            ;;
-        odm/etc/camera/aivsModel_6C06C006 | odm/lib64/libmialgo_aisn.so)
-            [ "$2" = "" ] && return 0
-            split --bytes=20M -d "${2}" "${2}".part
-            ;;
-        odm/etc/camera/enhance_motiontuning.xml |odm/etc/camera/night_motiontuning.xml | odm/etc/camera/motiontuning.xml)
-            [ "$2" = "" ] && return 0
-            sed -i 's/<?xml=/<?xml /g' "${2}"
-            ;;
         odm/lib64/hw/displayfeature.default.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
-            ;;
-        odm/lib64/libailab_rawhdr.so | odm/lib64/libxmi_high_dynamic_range_cdsp.so)
-            [ "$2" = "" ] && return 0
-            "${ANDROID_ROOT}"/prebuilts/clang/host/linux-x86/clang-r450784e/bin/llvm-strip --strip-debug "${2}"
             ;;
         odm/lib64/libmt@1.3.so)
             [ "$2" = "" ] && return 0

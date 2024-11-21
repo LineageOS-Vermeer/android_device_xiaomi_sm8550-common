3cc94d3d31d4bdbc0cd4243848f92b7288c780df
GioKas93 <giokas93@pm.me>
2024-11-21 16:40:08 +0100

    sm8550-common: Import missing permissions xml for WFD app
    *fixes:
    
    10-09 09:48:30.794  3365  3365 E AndroidRuntime: java.lang.IllegalStateException: Signature|privileged permissions not in privapp-permissions allowlist: {com.qualcomm.wfd.service (/system_ext/priv-app/WfdService): android.permission.START_FOREGROUND_SERVICES_FROM_BACKGROUND}
    10-09 09:48:30.794  3365  3365 E AndroidRuntime:   at com.android.server.pm.permission.PermissionManagerServiceImpl.onSystemReady(PermissionManagerServiceImpl.java:4447)
    10-09 09:48:30.794  3365  3365 E AndroidRuntime:   at com.android.server.pm.permission.PermissionManagerService$PermissionManagerServiceInternalImpl.onSystemReady(PermissionManagerService.java:743)
    10-09 09:48:30.794  3365  3365 E AndroidRuntime:   at com.android.server.pm.PackageManagerService.systemReady(PackageManagerService.java:4253)
    10-09 09:48:30.794  3365  3365 E AndroidRuntime:   at com.android.server.SystemServer.startOtherServices(SystemServer.java:2798)
    10-09 09:48:30.794  3365  3365 E AndroidRuntime:   at com.android.server.SystemServer.run(SystemServer.java:942)
    10-09 09:48:30.794  3365  3365 E AndroidRuntime:   at com.android.server.SystemServer.main(SystemServer.java:664)
    10-09 09:48:30.794  3365  3365 E AndroidRuntime:   at java.lang.reflect.Method.invoke(Native Method)
    10-09 09:48:30.794  3365  3365 E AndroidRuntime:   at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:552)
    10-09 09:48:30.794  3365  3365 E AndroidRuntime:   at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:949)
    10-09 09:48:30.795  3365  3365 I DropBoxManagerService: add tag=system_server_crash isTagEnabled=true flags=0x2

--- common.mk
+++ common.mk
@@ -426,7 +426,8 @@ PRODUCT_PACKAGES += \
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/permissions/wfd-system-ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/wfd-system-ext-privapp-permissions-qti.xml
 
 # Sensors
 PRODUCT_PACKAGES += \

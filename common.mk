0011948958d584df5a7dfde67b22696eb7f51297
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-11-26 13:51:32 +0100

    sm8550-common: Build libaudioroute to vendor
    Change-Id: I1374034a9cc9246eb382a5fbe764abeb9d5c5fe6

--- common.mk
+++ common.mk
@@ -89,6 +89,7 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
+    libaudioroute.vendor \
     libbatterylistener \
     libqcompostprocbundle \
     libqcomvisualizer \

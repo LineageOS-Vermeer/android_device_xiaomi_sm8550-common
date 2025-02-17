14c638461e27645a9ca31eb45cd1b4fd1c20b0f2
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-02-17 11:31:20 +0100

    sm8550-common: Drop Xiaomi touchfeature HAL
     * rework orientation service to use node
     * drop edge rejection part as its now done in kernel
    
    Change-Id: I77120e7d55745b7dc3011d71fb41455d7d9e62d8

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.java
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2024 Paranoid Android
+ * Copyright (C) 2023-2025 Paranoid Android
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -17,7 +17,6 @@ import android.view.Display;
 import android.view.Display.HdrCapabilities;
 
 import com.xiaomi.settings.display.ColorModeService;
-import com.xiaomi.settings.edgesuppression.EdgeSuppressionService;
 import com.xiaomi.settings.touch.TouchOrientationService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -44,8 +43,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
                 UserHandle.CURRENT);
 
         // Touchscreen
-        context.startServiceAsUser(new Intent(context, EdgeSuppressionService.class),
-                UserHandle.CURRENT);
         context.startServiceAsUser(new Intent(context, TouchOrientationService.class),
                 UserHandle.CURRENT);
         

353ea105049d9b6a771644016ef7d00162919ef4
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-05-27 13:04:50 +0200

    sm8550-common: Move Xiaomi Sensors HAL to select()
    Change-Id: Iffb04d8693fd8e425b6650632c7cf7ca146812fc

--- common.mk
+++ common.mk
@@ -454,6 +454,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+$(call soong_config_set, SENSORS_XIAOMI, USES_DOUBLE_TAP_SENSOR, true)
+$(call soong_config_set, SENSORS_XIAOMI, USES_SINGLE_TAP_SENSOR, true)
+$(call soong_config_set, SENSORS_XIAOMI, USES_UDFPS_SENSOR, true)
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \

a55b4d1e77f4c3d6e816d018239a6c1df969b1be
IPSBHANGU <inderpreetbhangu2@gmail.com>
2025-06-07 17:27:20 +0200

    fixup! sm8550-common: Sync extract-utils with template

--- extract-files.sh
+++ extract-files.sh
@@ -66,9 +66,11 @@ fi
 function blob_fixup() {
     case "${1}" in
         product/etc/permissions/vendor.qti.hardware.data.connectionaidl-V1-java.xml)
+            [ "$2" = "" ] && return 0
             sed -i 's/xml version="2.0"/xml version="1.0"/g' "${2}"
             ;;
         product/etc/permissions/vendor.qti.hardware.data.connection-V1*.xml)
+            [ "$2" = "" ] && return 0
             sed -i 's/xml version="2.0"/xml version="1.0"/g' "${2}"
            ;;
         odm/lib64/hw/displayfeature.default.so)

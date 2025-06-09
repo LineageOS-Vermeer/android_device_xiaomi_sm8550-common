8cb85fb108a3ea42daa56eaa6ef3376261b8ac30
IPSBHANGU <inderpreetbhangu2@gmail.com>
2025-06-09 12:14:11 +0200

    sm8550-common: add shim for libqcodec2_core for missing symbol _ZN15_C2FenceFactory15CreateSyncFenceEi

--- extract-files.py
+++ extract-files.py
@@ -226,6 +226,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('liblog.so'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
+    'vendor/lib64/libqcodec2_core.so': blob_fixup()
+        .add_needed('libcodec2_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
